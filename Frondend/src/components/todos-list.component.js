4e1e96809c6d6a3f1e1338b632bf56e42f846441
jojo-joseph <jojojoseph1996@gmail.com>
2019-07-05 19:06:59 +0530

    Todo initial commit

---
+++ Frondend/src/components/todos-list.component.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Todo = props => (
+    <tr>
+        <td>{props.todo.todo_discription}</td>
+        <td>{props.todo.todo_responsible}</td>
+        <td>{props.todo.todo_priority}</td>
+        <td>
+            <Link to={"/edit/"+props.todo._id}>Edit</Link>
+        </td>
+        <td>
+            <button>Delete</button>
+        </td>
+
+    </tr>
+)
+
+export default class TodosList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {todos: [] };
+      
+        //this.delete = this.delete.bind(this);
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/todos/')
+            .then(response => {
+                console.log(response.data);
+                this.setState({ todos: response.data });
+            })
+            .catch(function (error){
+                console.log(error);
+            })
+    }
+
+    todoList() {
+        return this.state.todos.map(function(currentTodo, i){
+            return <Todo todo={currentTodo} key={i} />;
+        })
+    }
+
+   
+    render() {
+        return (
+            <div>
+                <h3>Todos List</h3>
+                <table className="table table-striped" style={{ marginTop: 20 }} >
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Responsible</th>
+                            <th>Priority</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.todoList() }
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
